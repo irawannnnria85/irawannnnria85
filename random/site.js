80b4aec3ee1e62b43eb74d58d28e29f075f420b9
irawannnnria85 <95657874+irawannnnria85@users.noreply.github.com>
2021-12-08 02:53:15 +0700

    Create site.js

---
+++ random/site.js
@@ -0,0 +1,28 @@
+var pages = [ 
+
+"https://aridjayaadsterra.blogspot.com",
+"https://aridjayakeeper.blogspot.com",
+"https://aridjayamgid.blogspot.com",
+"https://clickbody.blogspot.com",
+"https://joinpayfazz.blogspot.com",
+"https://kipulsa.blogspot.com",
+"https://lancarbayar.blogspot.com",
+"https://lanjutinyuk.blogspot.com",
+"https://lanjutplay.blogspot.com",
+"https://lapakrecehan.blogspot.com",
+"https://lewatinsay.blogspot.com",
+"https://loadingsay.blogspot.com",
+"https://tempatagenpulsa.blogspot.com",
+"https://teruslanjutin.blogspot.com",
+"https://terusloading.blogspot.com",
+"https://theme-platinum.blogspot.com",
+"https://topindonesiya.blogspot.com",
+"https://topupbayar.blogspot.com",
+"https://travelppob.blogspot.com",
+"https://tunggu6detik.blogspot.com",
+
+]; 
+function randomPage() { 
+return pages[Math.round(Math.random() * (pages.length - 1))]; 
+} 
+location.href= randomPage(); 
